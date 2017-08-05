91bd170fbbf8104acafdee5507fc9509883f03fe
yusuke kakuma <meme@mamimumeme.com>
2017-08-05 16:38:44 +0900

    sasaコンパイル時エラー回避

--- config/webpack.sass.config.js
+++ config/webpack.sass.config.js
@@ -47,7 +47,8 @@ export default {
 							loader : 'css-loader',
 							options: {
 								minimize: true,
-								sourceMap: true
+								sourceMap: true,
+                                url: false
 							}
 						},
 						{
