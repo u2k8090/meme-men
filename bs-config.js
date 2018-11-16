d09eae88f76f626c6de9e0a10a5ce47ff4618ec5
yusuke kakuma <kakuma@mamimumeme.com>
2018-11-16 12:12:51 +0900

    bs-config 設定最適化

--- bs-config.js
+++ bs-config.js
@@ -1,18 +1,25 @@
-// php
-module.exports = {
-    proxy: "http://localhost:8000",
-    files: ["./wordpress/wp-content/themes/meme/assets/**/*","./wordpress/wp-content/themes/meme/**/*.php"],
-    open: 'external',
-    notify: true
+const { config } = require('./package.json');
+
+let wordpress = config.wordpress.enable;
+
+if(wordpress){
+  // wordpress site
+  module.exports = {
+      proxy: "http://localhost:8000",
+      files: ["./wordpress/wp-content/themes/meme/assets/**/*","./wordpress/wp-content/themes/meme/**/*.php"],
+      open: 'external',
+      notify: true
+  }
+} else {
+  // static site
+  module.exports = {
+      files: ["./dist/css/**/*.css", "./dist/js/**/*.js", "./**/*.html"],
+      server: {
+          "baseDir": "./"
+      },
+      startpath: 'index.html',
+      open: 'external',
+      notify: true,
+  }
 }
 
-// html
-// module.exports = {
-//     files: ["./dist/css/**/*.css", "./dist/js/**/*.js", "./**/*.html"],
-//     server: {
-//         "baseDir": "./"
-//     },
-//     startpath: 'index.html',
-//     open: 'external',
-//     notify: true,
-// }
